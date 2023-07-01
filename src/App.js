4edfa0a16aad41a8b1c1792401c9b83c10660d34
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-01 22:07:55 +0300

    Implemented basic Friends List and Friend components structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
 import './index.css';
+import { initialFriends } from './data';
 
-// import { initialFriends } from './data';
+const FriendsList = () => {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map(el => (
+        <Friend key={el.id} friendData={el} />
+      ))}
+    </ul>
+  );
+};
+
+const Friend = ({ friendData }) => {
+  return (
+    <li>
+      <img src={friendData.image} alt={friendData.name} />
+      <h3>{friendData.name}</h3>
+      {friendData.balance < 0 && (
+        <p className="red">
+          You owe {friendData.name} {Math.abs(friendData.balance)}€
+        </p>
+      )}
+      {friendData.balance > 0 && (
+        <p className="green">
+          {friendData.name} owe you {friendData.balance}€
+        </p>
+      )}
+      {friendData.balance === 0 && <p>You and {friendData.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+};
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
 };
 
 export default App;
