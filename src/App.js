f10806123b969368a8c6fc6c5728061684f97f4c
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-02 13:44:38 +0300

    Implemented friend adding functionality

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { useState } from 'react';
 import './index.css';
 import { initialFriends } from './data';
 
-const FriendsList = () => {
-  const friends = initialFriends;
-
+const FriendsList = ({ friends }) => {
   return (
     <ul>
       {friends.map(el => (
@@ -37,14 +35,47 @@ const Friend = ({ friendData }) => {
   );
 };
 
-const FormAddFriend = () => {
+const FormAddFriend = ({ onAddFriend }) => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  const nameInputChangeHandler = e => {
+    const inputName = e.target.value;
+    setName(inputName);
+  };
+
+  const imageInputChangeHandler = e => {
+    const inputImage = e.target.value;
+    setImage(inputImage);
+  };
+
+  const submitFriendHandler = e => {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const friendId = crypto.randomUUID();
+
+    const newFriend = {
+      name,
+      image: `${image}?=${friendId}`,
+      balance: 0,
+      id: friendId,
+    };
+
+    onAddFriend(newFriend);
+
+    setName('');
+    setImage('https://i.pravatar.cc/48');
+  };
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={submitFriendHandler}>
       <label>👯‍♀️ Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={nameInputChangeHandler} />
 
       <label>🎇 Image URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={imageInputChangeHandler} />
 
       <Button>Add</Button>
     </form>
@@ -85,17 +116,23 @@ const Button = ({ children, onClick }) => {
 };
 
 const App = () => {
+  const [friendsList, setFriendsList] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   const toggleShowFriendHandler = () => {
     setShowAddFriend(prevShow => !prevShow);
   };
 
+  const addFriendHandler = friendData => {
+    setFriendsList(prevList => [...prevList, friendData]);
+    setShowAddFriend(false);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friendsList} />
+        {showAddFriend && <FormAddFriend onAddFriend={addFriendHandler} />}
         <Button onClick={toggleShowFriendHandler}>
           {showAddFriend ? 'Close' : 'Add friend'}
         </Button>
